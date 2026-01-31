bf894567860f7271d80615fafba549cf865586b6
niltonjrdev <dev40.nilton@gmail.com>
2026-01-31 16:37:52 -0300

    criação da pasta buid para conversão dos arquivos .js

---
+++ db/migrations/20260125120627_add-session-id-to-transactions.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.up = up;
+exports.down = down;
+async function up(knex) {
+    await knex.schema.alterTable('transactions', (table) => {
+        table.uuid('session_id').after('id').index();
+    });
+}
+async function down(knex) {
+    await knex.schema.alterTable('transactions', (table) => {
+        table.dropColumn('session_id');
+    });
+}
+//# sourceMappingURL=20260125120627_add-session-id-to-transactions.js.map
