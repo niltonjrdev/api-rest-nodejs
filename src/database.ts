15015a9cddfab4240976e5095c87577ce9d18d8a
niltonjrdev <dev40.nilton@gmail.com>
2026-02-08 14:10:53 -0300

    "chore: preparar para deploy"

--- src/database.ts
+++ src/database.ts
@@ -6,8 +6,8 @@ export const config: Knex.Config = {
   connection: env.DATABASE_CLIENT === 'sqlite' 
   ? {
     filename: env.DATABASE_URL,
-  } : env.DATABASE_URL,
-
+    } 
+    : env.DATABASE_URL,
   useNullAsDefault: true,
   migrations: {
     extension: 'ts',
--- src/env/index.ts
+++ src/env/index.ts
@@ -2,7 +2,7 @@ import { config } from 'dotenv'
 import { z } from 'zod'
 
 if (process.env.NODE_ENV === 'test') {
-  config({path:'.env.test'})
+  config({ path: '.env.test' })
 } else {
   config()
 }
@@ -17,7 +17,7 @@ const envSchema = z.object({
 const _env = envSchema.safeParse(process.env)
 
 if (_env.success === false) {
-  console.error('⚠️ Invalid environment variables', _env.error.format()) // Adicione () aqui
+  console.error('⚠️ Invalid environment variables', _env.error.format())
 
   throw new Error('Invalid environment variables')
 }
